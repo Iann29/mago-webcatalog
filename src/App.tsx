12a6315c4a573e29e33d15abcf1a0e96cd340289
Iann29 <iansingshau@gmail.com>
2025-04-28 16:40:30 -0300

    Remoção da textura

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import TextureOverlay from './components/TextureOverlay';
 import Title from './pages/home/title';
 import Intro from './pages/home/intro';
 import Banca from './pages/home/banca';
@@ -9,8 +8,6 @@ import './styles/globals.css';
 function App() {
   return (
     <div className="app-container relative">
-      {/* Componente de textura de fundo */}
-      <TextureOverlay />
       
       {/* Container principal de conteúdo */}
       <div className="content-container w-full min-h-screen flex flex-col items-center fade-in" style={{ overflowX: 'hidden' }}>
